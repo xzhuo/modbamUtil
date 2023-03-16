b56790da9ce8483ec37b75dd0e5319f75128120a
xzhuo <yunkzo@gmail.com>
2023-03-16 15:38:11 -0500

    multi threading

--- bed.methyl.LD.py
+++ bed.methyl.LD.py
@@ -21,29 +21,46 @@ def process_line(line):
 
     return chrom, pos, methylation_set, unmethylation_set
 
-def process_input(input_file):
+# def process_input(input_file):
+#     out_list = []
+#     with open(input_file, 'r') as f:  # read the chrom size file
+#         last_line = ""
+#         for line in f.readlines():
+#             if last_line != "":
+#                 chrom, pos, met, unmet = process_line(line)
+#                 last_chrom, last_pos, last_met, last_unmet = process_line(last_line)
+#                 if chrom == last_chrom:
+#                     distance = int(pos) - int(last_pos)
+#                     both_met = len(met.intersection(last_met))
+#                     both_unmet = len(unmet.intersection(last_unmet))
+#                     unmet_met = len(met.intersection(last_unmet))
+#                     met_unmet = len(unmet.intersection(last_met))
+#                     depth = len(met) + len(unmet)
+#                     met_perc = len(met)/depth if depth > 0 else float("NaN")
+#                     fisher_ratio, fisher_p = stats.fisher_exact(table=[[both_met,unmet_met],[met_unmet,both_unmet]], alternative="greater")
+#                     log_fisher_p = -math.log10(fisher_p) + 0 if fisher_p > 0 else float("inf")  # get the log10 p value and add 0 to get rid of the -0.0 or return inf if p value is 0.
+#                     out_list.append([chrom, int(pos), distance, depth, met_perc, fisher_ratio, log_fisher_p])
+#             last_line = line
+
+#     return out_list
+
+def process_lines(last_line, line):
     out_list = []
-    with open(input_file, 'r') as f:  # read the chrom size file
-        last_line = ""
-        for line in f.readlines():
-            if last_line != "":
-                chrom, pos, met, unmet = process_line(line)
-                last_chrom, last_pos, last_met, last_unmet = process_line(last_line)
-                if chrom == last_chrom:
-                    distance = int(pos) - int(last_pos)
-                    both_met = len(met.intersection(last_met))
-                    both_unmet = len(unmet.intersection(last_unmet))
-                    unmet_met = len(met.intersection(last_unmet))
-                    met_unmet = len(unmet.intersection(last_met))
-                    depth = len(met) + len(unmet)
-                    met_perc = len(met)/depth if depth > 0 else float("NaN")
-                    fisher_ratio, fisher_p = stats.fisher_exact(table=[[both_met,unmet_met],[met_unmet,both_unmet]], alternative="greater")
-                    log_fisher_p = -math.log10(fisher_p) + 0 if fisher_p > 0 else float("inf")  # get the log10 p value and add 0 to get rid of the -0.0 or return inf if p value is 0.
-                    out_list.append([chrom, int(pos), distance, depth, met_perc, fisher_ratio, log_fisher_p])
-            last_line = line
+    chrom, pos, met, unmet = process_line(line)
+    last_chrom, last_pos, last_met, last_unmet = process_line(last_line)
+    if chrom == last_chrom:
+        distance = int(pos) - int(last_pos)
+        both_met = len(met.intersection(last_met))
+        both_unmet = len(unmet.intersection(last_unmet))
+        unmet_met = len(met.intersection(last_unmet))
+        met_unmet = len(unmet.intersection(last_met))
+        depth = len(met) + len(unmet)
+        met_perc = len(met)/depth if depth > 0 else float("NaN")
+        fisher_ratio, fisher_p = stats.fisher_exact(table=[[both_met,unmet_met],[met_unmet,both_unmet]], alternative="greater")
+        log_fisher_p = -math.log10(fisher_p) + 0 if fisher_p > 0 else float("inf")  # get the log10 p value and add 0 to get rid of the -0.0 or return inf if p value is 0.
+        out_list = [chrom, int(pos), distance, depth, met_perc, fisher_ratio, log_fisher_p]
 
     return out_list
-
 
 def main():
     parser = argparse.ArgumentParser(description='parser the pos bed like file to calculate methylation epiallele using LD or fisher exact test')
@@ -51,8 +68,6 @@ def main():
                         help='multi-threading')
     parser.add_argument('-i', '--input', type=str, required=True,
                         help='input bed like file with pos and read ID separated by methylation calls')
-    parser.add_argument('-w', '--window', type=int, default=10000000,
-                        help='processing window chunk size')
     parser.add_argument('-o', '--out', type=str, required=True,
                         help='output txt file storing the methylation epiallele linkage')
 
@@ -63,8 +78,16 @@ def main():
 
 
     start_time = time.time()
+    infile_array = []
+    out_list = []
+    with open(input, 'r') as f:  # read the bed file
+        for line in f.readlines():
+            infile_array.append(line.strip())
 
-    out_list = process_input(input_file)
+    # out_list = process_input(input_file)
+
+    with WorkerPool(n_jobs=args.threads) as pool:
+        outputs = pool.imap(process_lines, zip(infile_array[1:], infile_array[:-1]), iterable_len=len(infile_array) - 1, progress_bar=True)
     with open(args.out, "w") as out:
         for line in out_list:
             out.write("{:s}\t{:d}\t{:d}\t{:d}\t{:0.2f}\t{:0.2f}\t{:0.4f}\n".format(
