46ba8d8f3de9165d36a17c061f859f2fe75daa61
xzhuo <yunkzo@gmail.com>
2023-03-18 09:30:40 -0500

    single thread LD processing

--- bed.methyl.LD.py
+++ bed.methyl.LD.py
@@ -21,28 +21,28 @@ def process_line(line):
 
     return chrom, pos, methylation_set, unmethylation_set
 
-# def process_input(input_file):
-#     out_list = []
-#     with open(input_file, 'r') as f:  # read the chrom size file
-#         last_line = ""
-#         for line in f.readlines():
-#             if last_line != "":
-#                 chrom, pos, met, unmet = process_line(line)
-#                 last_chrom, last_pos, last_met, last_unmet = process_line(last_line)
-#                 if chrom == last_chrom:
-#                     distance = int(pos) - int(last_pos)
-#                     both_met = len(met.intersection(last_met))
-#                     both_unmet = len(unmet.intersection(last_unmet))
-#                     unmet_met = len(met.intersection(last_unmet))
-#                     met_unmet = len(unmet.intersection(last_met))
-#                     depth = len(met) + len(unmet)
-#                     met_perc = len(met)/depth if depth > 0 else float("NaN")
-#                     fisher_ratio, fisher_p = stats.fisher_exact(table=[[both_met,unmet_met],[met_unmet,both_unmet]], alternative="greater")
-#                     log_fisher_p = -math.log10(fisher_p) + 0 if fisher_p > 0 else float("inf")  # get the log10 p value and add 0 to get rid of the -0.0 or return inf if p value is 0.
-#                     out_list.append([chrom, int(pos), distance, depth, met_perc, fisher_ratio, log_fisher_p])
-#             last_line = line
+def process_input(input_file):
+    out_list = []
+    with open(input_file, 'r') as f:  # read the chrom size file
+        last_line = ""
+        for line in f.readlines():
+            if last_line != "":
+                chrom, pos, met, unmet = process_line(line)
+                last_chrom, last_pos, last_met, last_unmet = process_line(last_line)
+                if chrom == last_chrom:
+                    distance = int(pos) - int(last_pos)
+                    both_met = len(met.intersection(last_met))
+                    both_unmet = len(unmet.intersection(last_unmet))
+                    unmet_met = len(met.intersection(last_unmet))
+                    met_unmet = len(unmet.intersection(last_met))
+                    depth = len(met) + len(unmet)
+                    met_perc = len(met)/depth if depth > 0 else float("NaN")
+                    fisher_ratio, fisher_p = stats.fisher_exact(table=[[both_met,unmet_met],[met_unmet,both_unmet]], alternative="greater")
+                    log_fisher_p = -math.log10(fisher_p) + 0 if fisher_p > 0 else float("inf")  # get the log10 p value and add 0 to get rid of the -0.0 or return inf if p value is 0.
+                    out_list.append([chrom, int(pos), distance, depth, met_perc, fisher_ratio, log_fisher_p])
+            last_line = line
 
-#     return out_list
+    return out_list
 
 # def process_lines(last_line, line):
 #     out_list = []
@@ -98,16 +98,16 @@ def main():
 
 
     start_time = time.time()
-    infile_array = []
-    out_list = []
-    with open(input_file, 'r') as f:  # read the bed file
-        for line in f.readlines():
-            infile_array.append(line.strip())
+    out_list = process_input(input_file)
 
-    # out_list = process_input(input_file)
+    # infile_array = []
+    # out_list = []
+    # with open(input_file, 'r') as f:  # read the bed file
+    #     for line in f.readlines():
+    #         infile_array.append(line.strip())
 
-    with WorkerPool(n_jobs=args.threads, shared_objects=infile_array, start_method='fork') as pool:
-        out_list = pool.imap(process_arrays, range(1, len(infile_array)), iterable_len=len(infile_array) - 1, progress_bar=True)
+    # with WorkerPool(n_jobs=args.threads, shared_objects=infile_array, start_method='fork') as pool:
+    #     out_list = pool.imap(process_arrays, range(1, len(infile_array)), iterable_len=len(infile_array) - 1, progress_bar=True)
     with open(args.out, "w") as out:
         for line in out_list:
             if line == []:
