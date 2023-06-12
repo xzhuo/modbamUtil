3a8b4036e7847add9bf6c5c8a06cb3f2ee77da4c
xzhuo <yunkzo@gmail.com>
2023-06-12 17:26:52 -0500

    aggregate read methylation on locus in progress

--- read.methylation.mean.py
+++ read.methylation.mean.py
@@ -1,38 +1,82 @@
 import os
 import sys
 import argparse
+import time
 from mpire import WorkerPool
 
+class Locus:
+    def __init__(self, chr, start, end):
+        self.chr = chr
+        self.start = start
+        self.end = end
+        self.name = chr + ":" + start + "-" + end
+        self.reads = {}
+
+    def add_read(self, read):
+        self.reads[read.name] = read
+
+    def aggregate_methylation(self, length):
+        empty_b_cpgs = []
+        empty_a_cpgs = []
+        insertion_b_cpgs = []
+        insertion_a_cpgs = []
+        i_cpgs = []
+        number_of_reads = self.number_of_reads()
+        for read in self.reads:
+            read.methylation_list(length)
+            if read.readtype == "a,b":
+                empty_b_cpgs.extend(read.b_cpgs)
+                empty_a_cpgs.extend(read.a_cpgs)
+            elif read.readtype == "a,b,i":
+                i_cpgs.extend(read.i_cpgs)
+                insertion_b_cpgs.extend(read.b_cpgs)
+                insertion_a_cpgs.extend(read.a_cpgs)
+
+        self.empty_b_methylation = sum(empty_b_cpgs)/len(empty_b_cpgs)
+        self.empty_a_methylation = sum(empty_a_cpgs)/len(empty_a_cpgs)
+        self.insertion_b_methylation = sum(insertion_b_cpgs)/len(insertion_b_cpgs)
+        self.insertion_a_methylation = sum(insertion_a_cpgs)/len(insertion_a_cpgs)
+        self.i_methylation = sum(i_cpgs)/len(i_cpgs)
+        self.b_cpg_count = 
+        self.a_cpg_count = 
+        self.i_cpg_count = 
+
+    def get_read(self, read_name):
+        return self.reads[read_name]
+
+    def number_of_reads(self):
+        return len(self.reads)
+
 class Read:
-    def __init__(self, line):
-        self.chr, self.start, self.end, self.read, self.CGins, self.readtype = line.split()
+    def __init__(self, read, CGins, readtype):
+        self.name = read
+        self.CGins = CGins
+        self.readtype = readtype
+        self.locus = self.chr + ":" + self.start + "-" + self.end
         self.cpg = []
 
-    def add_cpg(self, pos, rel_pos, methylation, type):
-        cpg = {"pos": pos,"rel_pos": rel_pos, "methylation": methylation, "type": type}
+    def add_cpg(self, cpg):
         self.cpg.append(cpg)
 
-    def mean_methylation(self,type,length):
-        methylation_list = []
-        if type == "b":
-            methylation_list = [i["methylation"] for i in self.cpg if i["rel_pos"]<0 and i["rel_pos"]>length * -1 and pos > 0]
-        elif type == "a":
-            methylation_list = [i["methylation"] for i in self.cpg if i["rel_pos"]>0 and i["rel_pos"]<length and pos > 0]
-        elif type == "i":
-            methylation_list = [i["methylation"] for i in self.cpg if i["type"] == "i"]
-        else:
-            sys.exit("unknow region type, should be b, a, or i.")
+    def methylation_list(self,length):
+        self.b_cpgs = [i["methylation"] for i in self.cpg if i["rel_pos"]<0 and i["rel_pos"]>length * -1 and i["pos"] > 0]
+        self.a_cpgs = [i["methylation"] for i in self.cpg if i["rel_pos"]>0 and i["rel_pos"]<length and i["pos"] > 0]
+        self.i_cpgs = [i["methylation"] for i in self.cpg if i["type"] == "i"]
+        # mean_methylation = sum(methylation_list)/len(methylation_list) if len(methylation_list)>0 else -1
 
-        mean_methylation = sum(methylation_list)/len(methylation_list)
-        return mean_methylation
-
+class CpG:
+    def __init__(self, pos, rel_pos, methylation, type):
+        self.pos = pos
+        self.rel_pos = rel_pos
+        self.methylation = methylation
+        self.type = type
 
 def main():
     parser = argparse.ArgumentParser(description='Calculate mean methylation of both flanking and insertion regions of all reads')
     parser.add_argument('-p', '--position_file', type=str, required=True, help='input file with each CpG in all reads')
     parser.add_argument('-g', '--groupby_file', type=str, required=True, help='input file of all reads with read type annotation')
     parser.add_argument('-o', '--output', type=str, required=True, help='output file')
-    parser.add_argument('-l', '--len', type=int, default=50, help='length of flanking regions to average methylation')
+    parser.add_argument('-l', '--len', type=int, default=500, help='length of flanking regions to average methylation')
     parser.add_argument('-t', '--threads', type=int, default=1, help='multi-threading')
     args = parser.parse_args()
     position_file = os.path.abspath(args.position_file)
@@ -41,13 +85,49 @@ def main():
         raise ValueError("--position file does not exist!")
     if not os.path.exists(groupby_file):
         raise ValueError("--groupby file does not exist!")
-    output = os.path.abspath(args.output)
+    start_time = time.time()
 
-    read_dict = {}
+    locus_dict = {}
+    """
+    groupby file:
+    chr1	10861	10862	m64136_200710_174522/11207845/ccs	12	a,b
+    chr1	10861	10862	m64136_200710_174522/39323705/ccs	9	a,b
+    chr1	10861	10862	m64136_200711_235843/89718828/ccs	82	a,b
+    """
     with open(groupby_file, 'r') as f:  # read the region file
         for line in f.readlines():
-            line_item = Read(line)
-            read_dict[line_item.read] = line_item
+            chr, start, end, read, CGins, readtype = line.split()
+            locus_name = chr + ":" + start + "-" + end
+            read_item = Read(read, CGins, readtype)
+            if locus_name not in locus_dict:
+                locus_dict[locus_name] = Locus(chr, start, end)
+                locus_dict[locus_name].add_read(read_item)
+            else:
+                locus_dict[locus_name].add_read(read_item)
+
+    """
+    position file:
+    chr1	10861	10862	10082	m64136_200710_174522/11207845/ccs	-796	0.96	+	b
+    chr1	10861	10862	10207	m64136_200710_174522/11207845/ccs	-670	0.98	+	b
+    chr1	10861	10862	10468	m64136_200710_174522/11207845/ccs	-418	1.00	+	b
+    """
+    with open(position_file, 'r') as f:  # read the region file
+        for line in f.readlines():
+            chr, start, end, pos, read, rel_pos, methylation, strand, type = line.split()
+            cpg_item = CpG(pos, rel_pos, methylation, type)
+            locus = chr + ":" + start + "-" + end
+            if locus in locus_dict and read in locus_dict[locus].reads:
+                read = locus_dict[locus].get_read(read)
+                read.add_cpg(cpg_item)
+        
+    with open(args.output, "w") as out:
+        for locus in locus_dict:
+            locus_dict[locus].aggregate_methylation(args.len)
+            # chr, chr.start, chr,end, chr.pos, read, read.pos, methylation, strand
+            out.write("{:s}\t{:d}\t{:d}\t{:d}\t{:s}\t{:d}\t{:.2f}\t{:s}\n".format(
+                        i[0],i[1],i[2],i[3],i[4],i[5],i[6],i[7]))
+    end_time = time.time()
+    print("--- %s hours ---" % ((end_time - start_time)/3600))
 
 if __name__ == '__main__':
     main()
