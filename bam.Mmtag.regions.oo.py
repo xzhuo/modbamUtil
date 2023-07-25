1c541392d38349c51e9773ac421eb2135a41133a
xzhuo <yunkzo@gmail.com>
2023-07-25 14:52:12 -0500

    add read depth filter to remove cnv regions

--- bam.Mmtag.regions.oo.py
+++ bam.Mmtag.regions.oo.py
@@ -36,53 +36,57 @@ class Interval:
         else:
             sys.exit("unknow interval type!")
     
-    def attach_modbase_list(self, window, bam, len_offset):
+    def attach_modbase_list(self, window, bam, len_offset, depth_filter):
         flanking_window = (self.start - window if self.start > window else 0, self.end + window)
         self.modbase_list = []
+        reads = []
         for read in bam.fetch(self.chr, flanking_window[0], flanking_window[1]):
             if read.is_supplementary or read.is_secondary or read.is_unmapped:
                 continue
-            all_ref_pos = read.get_reference_positions()
-            read_ref_start = min(all_ref_pos, key=lambda x:abs(x-flanking_window[0]))
-            read_ref_end = min(all_ref_pos, key=lambda x:abs(x-flanking_window[1]))
-            ref_pos = min(all_ref_pos, key=lambda x:abs(x-self.start))
-            get_pos = convert_pos(read)
-            query_flanking_start = get_pos['find_query'][read_ref_start]
-            query_flanking_end = get_pos['find_query'][read_ref_end]
-            query_start = get_pos['find_query'][ref_pos]
-            insertion_regions = find_insertion(read)
-            try:
-                nearby_insertion = min(insertion_regions, key=lambda x:min(abs(x[0]-query_start), abs(x[1]-query_start)))
-                nearby_insertion = (-1,-1) if min(abs(nearby_insertion[0]-query_start), abs(nearby_insertion[1]-query_start)) > len_offset else nearby_insertion
-            except:
-                nearby_insertion = (-1,-1)
-            query_name = read.query_name
-            modbase_key = ('C', 1, 'm') if read.is_reverse else ('C', 0, 'm')
-            strand = '-' if read.is_reverse else '+'
-            try:
-                modbase_list = read.modified_bases[modbase_key]
-                modbase_query_list = [j[0] for j in list(filter(lambda i: i[0] >= query_flanking_start and i[0] < query_flanking_end, modbase_list))]
-                modbase_ref_list = [get_pos['find_ref'][i] if i in get_pos['find_ref'] else -1 for i in modbase_query_list]
-                modbase_rel_pos_list = [i - query_start for i in modbase_query_list]
-                abi_list = []
-                if nearby_insertion != (-1,-1):
-                    abi_type = "insertion"
-                    for i in range(len(modbase_query_list)):
-                        if modbase_query_list[i] < nearby_insertion[0]:
-                            abi_list.append("b")
-                        elif modbase_query_list[i] >= nearby_insertion[0] and modbase_query_list[i] < nearby_insertion[1]:
-                            abi_list.append("i")
-                        else:
-                            abi_list.append("a")
-                else:
-                    abi_type = "empty"
-                    abi_list = ["b" if i< query_start else "a" for i in modbase_query_list]
-                modbase_perc_list = [j[1]/255 for j in list(filter(lambda i: i[0] >= query_flanking_start and i[0] < query_flanking_end, modbase_list))]
-                modbase_pos_perc = list(zip(modbase_rel_pos_list, modbase_ref_list, modbase_perc_list, abi_list))
-            except:
-                modbase_pos_perc = []
-                abi_type = "absent"
-            self.modbase_list.append({'read': query_name, 'strand': strand, 'abi_type': abi_type, 'modbase_pos_perc': modbase_pos_perc})
+            reads.append(read)
+        if len(reads) < depth_filter:
+            for read in reads:
+                all_ref_pos = read.get_reference_positions()
+                read_ref_start = min(all_ref_pos, key=lambda x:abs(x-flanking_window[0]))
+                read_ref_end = min(all_ref_pos, key=lambda x:abs(x-flanking_window[1]))
+                ref_pos = min(all_ref_pos, key=lambda x:abs(x-self.start))
+                get_pos = convert_pos(read)
+                query_flanking_start = get_pos['find_query'][read_ref_start]
+                query_flanking_end = get_pos['find_query'][read_ref_end]
+                query_start = get_pos['find_query'][ref_pos]
+                insertion_regions = find_insertion(read)
+                try:
+                    nearby_insertion = min(insertion_regions, key=lambda x:min(abs(x[0]-query_start), abs(x[1]-query_start)))
+                    nearby_insertion = (-1,-1) if min(abs(nearby_insertion[0]-query_start), abs(nearby_insertion[1]-query_start)) > len_offset else nearby_insertion
+                except:
+                    nearby_insertion = (-1,-1)
+                query_name = read.query_name
+                modbase_key = ('C', 1, 'm') if read.is_reverse else ('C', 0, 'm')
+                strand = '-' if read.is_reverse else '+'
+                try:
+                    modbase_list = read.modified_bases[modbase_key]
+                    modbase_query_list = [j[0] for j in list(filter(lambda i: i[0] >= query_flanking_start and i[0] < query_flanking_end, modbase_list))]
+                    modbase_ref_list = [get_pos['find_ref'][i] if i in get_pos['find_ref'] else -1 for i in modbase_query_list]
+                    modbase_rel_pos_list = [i - query_start for i in modbase_query_list]
+                    abi_list = []
+                    if nearby_insertion != (-1,-1):
+                        abi_type = "insertion"
+                        for i in range(len(modbase_query_list)):
+                            if modbase_query_list[i] < nearby_insertion[0]:
+                                abi_list.append("b")
+                            elif modbase_query_list[i] >= nearby_insertion[0] and modbase_query_list[i] < nearby_insertion[1]:
+                                abi_list.append("i")
+                            else:
+                                abi_list.append("a")
+                    else:
+                        abi_type = "empty"
+                        abi_list = ["b" if i< query_start else "a" for i in modbase_query_list]
+                    modbase_perc_list = [j[1]/255 for j in list(filter(lambda i: i[0] >= query_flanking_start and i[0] < query_flanking_end, modbase_list))]
+                    modbase_pos_perc = list(zip(modbase_rel_pos_list, modbase_ref_list, modbase_perc_list, abi_list))
+                except:
+                    modbase_pos_perc = []
+                    abi_type = "absent"
+                self.modbase_list.append({'read': query_name, 'strand': strand, 'abi_type': abi_type, 'modbase_pos_perc': modbase_pos_perc})
 
 
 def convert_pos(read):
@@ -107,14 +111,14 @@ def find_insertion(read, soft_clip=True, length=50):
             curr_pos += i[1]
     return insertions
 
-def intersect_methylation(bam_file, interval, window, len_offset):
+def intersect_methylation(bam_file, interval, window, len_offset, depth_filter=100):
     """
     summarize the methylation of each CpG per read in the defined regions
     """
 
     out_list = []
     bam = pysam.AlignmentFile(bam_file, threads = 4, check_sq=False)
-    interval.attach_modbase_list(window, bam, len_offset)
+    interval.attach_modbase_list(window, bam, len_offset, depth_filter)
     for i in interval.modbase_list:
         for j in i['modbase_pos_perc']:
             # chr, chr.start, chr,end, chr.pos, read, read.pos, methylation, strand
@@ -139,6 +143,8 @@ def main():
                         help='The file format of the genomic region file, only vcf or bed like files are supported.')
     parser.add_argument('-l', '--len_offset', type=int, default=20,
                         help='length and coordinate offset from vcf to the bam file. Only insertions with coordinate offset < len and SVlength difference < len will be considered')
+    parser.add_argument('-d', '--depth_filter', type=int, default=100,
+                        help='regions with read depth higher than the threshold will be ignored')
     parser.add_argument('-w', '--window', type=int, default=2000,
                         help='flanking window on both ends of identified insrtions of the vcf file. CpG methylation within this window on both ends will be summarized')
     parser.add_argument('-o', '--out', type=str, required=True,
@@ -164,13 +170,13 @@ def main():
     if args.threads == 1:
         with open(args.out, "w") as out:
             for j in interval_array:
-                output_list = intersect_methylation(bam_file, j, args.window, args.len_offset)
+                output_list = intersect_methylation(bam_file, j, args.window, args.len_offset, args.depth_filter)
                 for i in output_list:
                     out.write("{:s}\t{:d}\t{:d}\t{:d}\t{:s}\t{:d}\t{:.2f}\t{:s}\t{:s}\t{:s}\n".format(
                             i[0],i[1],i[2],i[3],i[4],i[5],i[6],i[7],i[8],i[9]))
     else:
         with WorkerPool(n_jobs=args.threads) as pool:
-            outputs = pool.imap(intersect_methylation, zip(repeat(bam_file), interval_array, repeat(args.window), repeat(args.len_offset)), iterable_len=len(interval_array), progress_bar=True)
+            outputs = pool.imap(intersect_methylation, zip(repeat(bam_file), interval_array, repeat(args.window), repeat(args.len_offset), repeat(args.depth_filter)), iterable_len=len(interval_array), progress_bar=True)
 
         with open(args.out, "w") as out:
             for out_list in outputs:
